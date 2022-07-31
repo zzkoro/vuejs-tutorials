9073f15739e7b3aa3c8cb53d6221068dd2ae1e2c
junemp <junemp@gmail.com>
2022-08-01 08:36:37 +0900

    수정 적용

--- vuetify-template/src/views/authentication/SignIn.vue
+++ vuetify-template/src/views/authentication/SignIn.vue
@@ -1,15 +1,102 @@
 <template lang="">
-  <v-container>
-    <v-row>
-      <v-col>
-        <v-card>Sign In</v-card>
+  <v-container class="fill-height">
+    <v-row justify="center">
+      <v-col cols="auto">
+        <v-card width="460">
+          <v-card-text class="text-center px-12 py-16">
+            <validation-observer
+              ref="observer"
+              v-slot="{ invalid }"
+            >
+              <v-form
+                ref="form"
+                @submit.prevent="signIn"
+              >
+                <div class="text-h4 font-weight-black mb-10">
+                  로그인
+                </div>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="이메일"
+                  :rules="{
+                    required: true,
+                  }"
+                >
+                  <v-text-field
+                    v-model="email"
+                    label="이메일"
+                    clearable
+                    prepend-icon="mdi-email"
+                    :error-messasges="errors"
+                  />
+                </validation-provider>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="비밀번호"
+                  :rules="{
+                    required: true,
+                  }"
+                >
+                  <v-text-field
+                    v-model="password"
+                    label="비밀번호"
+                    clearable
+                    prepend-icon="mdi-lock-outline"
+                    :error-messasges="errors"
+                  />
+                </validation-provider>
+                <v-btn
+                  class="mt-6"
+                  type="submit"
+                  block
+                  x-large
+                  rounded
+                  color="primary"
+                  :disabled="invalid"
+                >
+                  로그인
+                </v-btn>
+                <div class="mt-5">
+                  <router-link
+                    class="text-decoration-none"
+                    to="/"
+                  >
+                    홈
+                  </router-link> |
+                  <router-link
+                    class="text-decoration-none"
+                    to="/authentication/sign-up"
+                  >
+                    회원가입
+                  </router-link>
+                </div>
+              </v-form>
+            </validation-observer>
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 <script>
 export default {
-  name: 'SignIn'
+  name: 'SignIn',
+  data() {
+    return {
+      email: null,
+      password: null
+    }
+  },
+  methods: {
+    signIn() {
+      this.$refs.observer.validate().then(result => {
+        if (result) {
+          alert('로그인 처리')
+        }
+      })
+
+    }
+  },
 }
 </script>
 <style lang="">
